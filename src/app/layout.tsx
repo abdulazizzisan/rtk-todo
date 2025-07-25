9fa581d6dbbaf17c09fc0ad72112ea4bda15ddfb
abdulazizzisan <binabdulaziz4983@gmail.com>
2025-07-25 23:53:51 +0600

    add theme provider in root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppDock } from "@/components/AppDock";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
-        <AppDock/>
+        <ThemeProvider
+        attribute={"class"}
+        defaultTheme={"system"}
+        enableSystem
+        disableTransitionOnChange
+        storageKey={"theme"}
+        >
+          <main>{children}</main>
+          <AppDock />
+        </ThemeProvider>
       </body>
     </html>
   );
