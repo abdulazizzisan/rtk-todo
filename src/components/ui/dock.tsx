23065ce5f9b0ace279ac2702a367797563137018
abdulazizzisan <binabdulaziz4983@gmail.com>
2025-07-26 13:41:58 +0600

    fix type error in dock.tsx

--- src/components/ui/dock.tsx
+++ src/components/ui/dock.tsx
@@ -9,7 +9,7 @@ import {
   type SpringOptions,
   AnimatePresence,
 } from 'framer-motion';
-import {
+import React, {
   Children,
   cloneElement,
   createContext,
@@ -160,7 +160,7 @@ function DockItem({ children, className }: DockItemProps) {
       aria-haspopup='true'
     >
       {Children.map(children, (child) =>
-        cloneElement(child as React.ReactElement, { width, isHovered })
+        cloneElement(child as React.ReactElement, { width, isHovered } as object)
       )}
     </motion.div>
   );
