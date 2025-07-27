cc9a6b31df9369c90aa8a46d3557aee4575f7b6f
abdulazizzisan <binabdulaziz4983@gmail.com>
2025-07-27 17:20:00 +0600

    RTK Query setup for fetching todo from jsonplaceholder

---
+++ src/lib/redux/features/todos/todoApiSlice.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const todoApi = createApi({
+  reducerPath: "todoApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/todos",
+    }),
+  }),
+});
+
+export const { useGetTodosQuery } = todoApi;
+export default todoApi;
--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,13 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todos/todosSlice";
+import todoApi from "./features/todos/todoApiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const makeStore = () => {
-  return configureStore({
+  const store = configureStore({
     reducer: {
       todos: todoReducer,
+      [todoApi.reducerPath]: todoApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(todoApi.middleware),
   });
+  setupListeners(store.dispatch);
+  return store;
 };
+
 
 // infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
